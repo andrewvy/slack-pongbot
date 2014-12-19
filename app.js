9ed3c410ced368226e01826a8d171c45916f410c
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-12-18 21:19:24 -0500

    changed channel, cleanup

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ mongoose.connect(mongoUri);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// var db = require('./lib/db');
 var pong = require('./lib/pong');
 var routes = require('./lib/routes');
 
@@ -45,14 +44,14 @@ app.post('/commands', routes.commands);
 
 app.get('/api/rankings', function(req, res) {
 	Player.find({}).sort({'elo': 'descending'}).find( function(err, players) {
-		if (err) return console.error(err);
+		if (err) return res.status(500).send(err);
 		res.json(players);
 	});
 });
 
 app.get('/api/matches', function(req, res) {
 	Challenge.find({}).sort({'date': 'desc'}).find( function(err, challenges) {
-		if (err) return console.error(err);
+		if (err) return res.status(500).send(err);
 		res.json(challenges);
 	});
 });
