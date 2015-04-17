aeca5cfda7ac323b0d843a45cb2a7fd5c414a14b
dblock <dblock@dblock.org>
2015-04-17 14:31:48 -0400

    Added jshint and fixed variable scoping issues.

--- test/pong_tests.js
+++ test/pong_tests.js
@@ -1,37 +1,37 @@
-var expect = require("chai").expect;
-var pong = require("../lib/pong.js");
-var Player = require("../models/Player");
-var mongoose = require("mongoose");
-var sinon = require("sinon");
+var expect = require('chai').expect;
+var pong = require('../lib/pong.js');
+var Player = require('../models/Player');
+var mongoose = require('mongoose');
+var sinon = require('sinon');
 
-describe("Pong", function() {
-  before(function(done) {
+describe('Pong', function () {
+  before(function (done) {
     pong.init();
     mongoose.connect('mongodb://localhost/pingpong_test', done);
   });
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     mongoose.connection.db.dropDatabase(done);
   });
 
-  describe("#init()", function() {
-    it("sets channel", function() {
+  describe('#init()', function () {
+    it('sets channel', function () {
       expect(pong.channel).to.eq('#pongbot');
     });
-    it("sets deltaTau", function() {
+    it('sets deltaTau', function () {
       expect(pong.deltaTau).to.eq(0.94);
     });
-    it("unsets currentChallenge", function() {
+    it('unsets currentChallenge', function () {
       expect(pong.currentChallenge).to.be.false;
     });
   });
 
-  describe("#registerPlayer", function() {
-    beforeEach(function(done) {
+  describe('#registerPlayer', function () {
+    beforeEach(function (done) {
       pong.registerPlayer('ZhangJike', done);
     });
 
-    it("creates a player record", function(done) {
+    it('creates a player record', function (done) {
       Player.where({ user_name: 'ZhangJike' }).findOne(function (err, user) {
         expect(user).not.to.be.null;
         expect(user.user_name).to.eq('ZhangJike');
@@ -44,23 +44,24 @@ describe("Pong", function() {
     });
   });
 
-  describe("#findPlayer", function() {
-    describe("with a player", function() {
-      beforeEach(function(done) {
+  describe('#findPlayer', function () {
+    describe('with a player', function () {
+      beforeEach(function (done) {
         pong.registerPlayer('ZhangJike', done);
       });
 
-      it("finds a player", function(done) {
-        pong.findPlayer('ZhangJike', function(user) {
+      it('finds a player', function (done) {
+        pong.findPlayer('ZhangJike', function (user) {
           expect(user).not.to.be.null;
           expect(user.user_name).to.eq('ZhangJike');
           done();
         });
       });
     });
-    describe("without a player", function() {
-      it("doesn't find player", function(done) {
-        pong.findPlayer('ZhangJike', function(user) {
+
+    describe('without a player', function () {
+      it('doesn\'t find player', function (done) {
+        pong.findPlayer('ZhangJike', function (user) {
           expect(user).to.be.false;
           done();
         });
@@ -68,89 +69,74 @@ describe("Pong", function() {
     });
   });
 
-  describe("getEveryone", function() {
-    describe("with a player", function() {
-      beforeEach(function(done) {
+  describe('getEveryone', function () {
+    describe('with a player', function () {
+      beforeEach(function (done) {
         sinon.spy(console, 'log');
-        pong.registerPlayer('ZhangJike', function() {
+        pong.registerPlayer('ZhangJike', function () {
           pong.getEveryone(done);
         });
       });
+
       afterEach(function () {
         console.log.restore();
       });
-      it("logs user", function() {
+
+      it('logs user', function () {
         expect(console.log.calledOnce).to.be.true;
         expect(console.log.firstCall.args[0][0].user_name).to.eq('ZhangJike');
       });
     });
   });
 
-  describe("updateWins", function() {
-
+  describe('updateWins', function () {
   });
 
-  describe("updateLosses", function() {
-
+  describe('updateLosses', function () {
   });
 
-  describe("reateSingleChallenge", function() {
-
+  describe('reateSingleChallenge', function () {
   });
 
-  describe("createDoubleChallenge", function() {
-
+  describe('createDoubleChallenge', function () {
   });
 
-  describe("checkChallenge", function() {
-
+  describe('checkChallenge', function () {
   });
 
-  describe("setChallenge", function() {
-
+  describe('setChallenge', function () {
   });
 
-  describe("removeChallenge", function() {
-
+  describe('removeChallenge', function () {
   });
 
-  describe("acceptChallenge", function() {
-
+  describe('acceptChallenge', function () {
   });
 
-  describe("declineChallenge", function() {
-
+  describe('declineChallenge', function () {
   });
 
-  describe("calculateTeamElo", function() {
-
+  describe('calculateTeamElo', function () {
   });
 
-  describe("eloSinglesChange", function() {
-
+  describe('eloSinglesChange', function () {
   });
 
-  describe("eloDoublesChange", function() {
-
+  describe('eloDoublesChange', function () {
   });
 
-  describe("win", function() {
-
+  describe('win', function () {
   });
 
-  describe("lose", function() {
-
+  describe('lose', function () {
   });
 
-  describe("findDoublesPlayers", function() {
-
+  describe('findDoublesPlayers', function () {
   });
 
-  describe("reset", function() {
-
+  describe('reset', function () {
   });
 
-  describe("getDuelGif", function() {
-
+  describe('getDuelGif', function () {
   });
 });
