c8a214d364aa1f7e1439ed94b990567375897f40
dB <dblock@dblock.org>
2015-04-26 09:52:38 -0400

    Support @player.

--- models/Player.js
+++ models/Player.js
@@ -3,12 +3,13 @@ var mongoosePages = require('mongoose-pages');
 var Schema = mongoose.Schema;
 
 var PlayerSchema = new Schema({
-	user_name: { type: String, index: { unique: true }, required: true },
-	wins: Number,
-	losses: Number,
-	elo: Number,
-	tau: Number,
-	currentChallenge: { type: Schema.Types.ObjectId, ref: 'Challenge' }
+  user_id: String,
+  user_name: { type: String, index: { unique: true }, required: true },
+  wins: Number,
+  losses: Number,
+  elo: Number,
+  tau: Number,
+  currentChallenge: { type: Schema.Types.ObjectId, ref: 'Challenge' }
 });
 
 mongoosePages.anchor(PlayerSchema);
@@ -17,6 +18,7 @@ PlayerSchema.methods = {
   halJSON: function (req) {
     return {
       data: {
+        user_id: this.user_id,
         user_name: this.user_name,
         wins: this.wins,
         losses: this.losses,
